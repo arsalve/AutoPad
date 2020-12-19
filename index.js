8e74f5005b4740f17fec50d023e4a2a535b2aecc
Ashish R Salve <46025867+arsalve@users.noreply.github.com>
2020-12-19 21:42:07 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require('express')
+const app = express();
+const yargs = require('yargs')
+const chalk = require('chalk');
+const ErrorC = chalk.red.inverse;
+const Warning = chalk.yellowBright;
+const suc = chalk.greenBright;
+var port;
+var debug = true;
+if ((yargs.argv.Port != undefined) && (typeof (yargs.argv.Port) == "number")) {
+    port = yargs.argv.Port
+} else {
+    console.log(Warning("Port not found so tracking targetes on usual location"))
+    port = 80;
+}
+try {
+
+    app.get('/', (req, res) => {
+        res.send('Lets Lift off')
+    })
+
+    app.listen(port, () => {
+        console.log(suc(`ready for targates on  http://localhost:${port}`));
+    })
+} catch (error) {
+    catchHandler("starting the server", error);
+
+}
+
+
+
+
+function catchHandler(location, message, color) {
+    if (debug = true) {
+        if (color == undefined) {
+            color = chalk.blueBright;
+        }
+        console.error(color("error occured when " + location + "\n" + message))
+    }
+}
