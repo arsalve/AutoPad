31ec29e46aec4a1ad88415536169939be18d80ad
Ashish Salve <a.r.salve@gmail.com>
2021-02-11 23:47:41 +0530

    Prompt fix

--- Public/Scripts/Script.js
+++ Public/Scripts/Script.js
@@ -1,10 +1,9 @@
    if (window.location.href.includes("localhost:8080")) {
        var url = "http://localhost:8080"
-   } else if(window.location.href.includes('https://project-notepad.herokuapp.com')) {
+   } else if (window.location.href.includes('https://project-notepad.herokuapp.com')) {
        var url = "https://project-notepad.herokuapp.com"
-   }
-   else if(window.location.href.includes('http://autopad.dinafo.com')){
-       var url='http://autopad.dinafo.com';
+   } else if (window.location.href.includes('http://autopad.dinafo.com')) {
+       var url = 'http://autopad.dinafo.com';
    }
    // Importing 'crypto' module 
 
@@ -13,9 +12,9 @@
    function load_text() {
        var Hash;
        Hash = sessionStorage.getItem('Hash');
-       var PWD = prompt("Enter Password")
+       var PWD;
+       sessionStorage.getItem('Password') == null ? PWD = prompt("Enter Password") : PWD = sessionStorage.getItem('Password');
        if (Hash == null) {
-          
            sessionStorage.setItem('Hash', stringToHash(PWD));
            Hash = sessionStorage.getItem('Hash');
            sessionStorage.setItem('Password', PWD);
@@ -29,7 +28,7 @@
 
                if (this.readyState === 4) {
 
-                if (this.response != "object not Found"||this.response != "error"||this.response != "Error") {
+                   if (this.response != "object not Found" || this.response != "error" || this.response != "Error") {
                        var responseData = JSON.parse(this.response)[0];
                        document.getElementById("Instruction").hidden = true;
                        document.getElementById("UpdateStatus").hidden = false;
@@ -55,7 +54,7 @@
            sessionStorage.clear();
            document.location.hash = Date.now();
            document.getElementById("output").value = "Use " + document.location + " to access the data on any device";
-        
+
            sessionStorage.setItem('Hash', stringToHash(PWD));
            Hash = sessionStorage.getItem('Hash');
            sessionStorage.setItem('Password', PWD);
@@ -64,44 +63,44 @@
    }
 
    function Update() {
+       
        var Hash, OGHash;
        try {
            Hash = sessionStorage.getItem('Hash');
            OGHash = sessionStorage.getItem("OGHash");
-           if(Hash==undefined||OGHash==undefined||Hash==null||OGHash==null){
-            var xhr = new XMLHttpRequest();
-            xhr.addEventListener("readystatechange", function () {
- 
-                if (this.readyState === 4) {
-                    if (this.response != "object not Found"||this.response != "error"||this.response != "Error") {
-                        var responseData = JSON.parse(this.response)[0];
-                        document.getElementById("Instruction").hidden = true;
-                        document.getElementById("UpdateStatus").hidden = false;
-                        document.getElementById("UpdateStatusText").innerHTML = 'Last Updated: ' + new Date(responseData.updatedAt);
-                        Decrypted = CryptoJS.AES.decrypt(responseData.data, sessionStorage.getItem('Password'))
-                        if (responseData.Encrypted == true) {
-                            sessionStorage.setItem("OGHash", responseData.Hash)
-                        }
-                        console.log(responseData);
-                    }
- 
-                } else {
-                    document.getElementById("Instruction").hidden = false;
-                    document.getElementById("UpdateStatus").hidden = true;
-                }
-            });
-            xhr.open("POST", url + "/find");
-            xhr.setRequestHeader("Content-Type", "application/json");
-            xhr.send(data);
-            var PWD = prompt("Enter Password")
-            sessionStorage.setItem('Hash', stringToHash(PWD));
-            Hash = sessionStorage.getItem('Hash');
-            sessionStorage.setItem('Password', PWD);
+           if (Hash == undefined || OGHash == undefined || Hash == null || OGHash == null) {
+               var xhr = new XMLHttpRequest();
+               xhr.addEventListener("readystatechange", function () {
+
+                   if (this.readyState === 4) {
+                       if (this.response != "object not Found" || this.response != "error" || this.response != "Error") {
+                           var responseData = JSON.parse(this.response)[0];
+                           document.getElementById("Instruction").hidden = true;
+                           document.getElementById("UpdateStatus").hidden = false;
+                           document.getElementById("UpdateStatusText").innerHTML = 'Last Updated: ' + new Date(responseData.updatedAt);
+                           Decrypted = CryptoJS.AES.decrypt(responseData.data, sessionStorage.getItem('Password'))
+                           if (responseData.Encrypted == true) {
+                               sessionStorage.setItem("OGHash", responseData.Hash)
+                           }
+                           console.log(responseData);
+                       }
+
+                   } else {
+                       document.getElementById("Instruction").hidden = false;
+                       document.getElementById("UpdateStatus").hidden = true;
+                   }
+               });
+               xhr.open("POST", url + "/find");
+               xhr.setRequestHeader("Content-Type", "application/json");
+               xhr.send(data);
+               var PWD = prompt("Enter Password")
+               sessionStorage.setItem('Hash', stringToHash(PWD));
+               Hash = sessionStorage.getItem('Hash');
+               sessionStorage.setItem('Password', PWD);
            }
-       }
-       catch(e) {
-        console.log(e + 'Due to which we are unable to process  data');
-          
+       } catch (e) {
+           console.log(e + 'Due to which we are unable to process  data');
+
        }
 
        var Encrypted = CryptoJS.AES.encrypt(document.getElementById("output").value, sessionStorage.getItem('Password'));
