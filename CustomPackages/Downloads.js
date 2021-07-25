5d92064bdfc6f0f5af5c1d94e3d4653212a54d93
Ashish R Salve <a.r.salve@gmail.com>
2021-07-25 13:15:01 +0530

    trying a fix

--- CustomPackages/Downloads.js
+++ CustomPackages/Downloads.js
@@ -64,7 +64,7 @@ function YoutubeDL(req, cb) {
 
 //Following function fetches the URL from instagram and sends the link as responce
 
-async function InstaDL(req, cb) {
+ function InstaDL(req, cb) {
     var re = 0
     var video_url = req.body.url;
     try {
@@ -75,13 +75,13 @@ async function InstaDL(req, cb) {
             if(video_url.substring(0,8) === 'https://' || video_url.substring(0,7) === 'http://' 
                     || video_url.substring(0,21) === 'https://www.instagram' || video_url.substring(0,20) === 'http://www.instagram.com'){
     
-                request(video_url, (error, response, html) => {
+                 request(video_url, (error, response, html) => {
                     if(!error){
                         console.log('Insta_grab : '+video_url+' : Loaded');
                         let $ = cheerio.load(html);
     
                         //basic data from the meta tags
-                        let video_link = $('meta[property="og:video"]').attr('content');
+                        let video_link =  $('meta[property="og:video"]').attr('content');
                         let file = $('meta[property="og:type"]').attr('content');
                         let url = $('meta[property="og:url"]').attr('content');
                         let title = $('meta[property="og:title"]').attr('content');
@@ -90,14 +90,14 @@ async function InstaDL(req, cb) {
                         return cb(video_link);
                         
                     }else{
-                        res.status(400).json({ 'message' : 'Error, Unable to load webpage'});
+                        cb({ 'message' : 'Error, Unable to load webpage'});
                     }
                 });
             }else{
-                res.status(201).json({ 'message' : 'Invalid URL'});
+                cb({ 'message' : 'Invalid URL'});
             }
         }else{
-            res.status(400).json({ 'message' : 'Provided invalid URL'});
+            cb({ 'message' : 'Provided invalid URL'});
         }
         // puppeteer.launch({
         //     headless: false
